602bafd42929d9a1ed6517a985556164bcee521d
Sebastian Borza <sebito91@gmail.com>
2018-09-11 17:42:48 -0500

    adding in kafka service and processors

--- connection/connection.go
+++ connection/connection.go
@@ -5,10 +5,12 @@ type ID string
 
 // Connection is a configuration used by the connector to transport data from source to the EG (target).
 type Connection struct {
-	Space  string           `json:"space"`
-	ID     ID               `json:"connectionId"`
-	Target string           `json:"target"`
-	Source AWSKinesisSource `json:"source"`
+	Space            string           `json:"space"`
+	ID               ID               `json:"connectionId"`
+	Target           string           `json:"target"`
+	Type             string           `json:"type"`
+	AWSKinesisSource AWSKinesisSource `json:"AWSKinesisSource,omitempty"`
+	KafkaSource      KafkaSource      `json:"KafkaSource,omitempty"`
 }
 
 // AWSKinesisSource is a configuration used to configure AWS Kinesis stream as a source.
@@ -18,4 +20,12 @@ type AWSKinesisSource struct {
 	AWSAccessKeyID     string `json:"awsAccessKeyId,omitempty"`
 	AWSSecretAccessKey string `json:"awsSecretAccessKey,omitempty"`
 	AWSSessionToken    string `json:"awsSessionToken,omitempty"`
+}
+
+// KafkaSource is the configuration struct to leverage when defining a Kafka resource
+// TODO: add tls config portion
+type KafkaSource struct {
+	Topic      string   `json:"topic"`
+	Partitions int      `json:"partitions"`
+	Brokers    []string `json:"hosts"`
 }
