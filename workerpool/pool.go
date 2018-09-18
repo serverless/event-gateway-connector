984a125955315999b103e5a29eac471332eddbe7
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-09-18 15:31:55 +0200

    Refactor WorkerPool; more logic in job struct; use of WaitGroup

--- workerpool/pool.go
+++ workerpool/pool.go
@@ -2,6 +2,7 @@ package workerpool
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/coreos/etcd/clientv3/concurrency"
@@ -33,86 +34,36 @@ func New(session *concurrency.Session, maxWorkers uint, events <-chan *watcher.E
 		log:        log,
 		jobs:       make(map[connection.ID]*job),
 		events:     events,
-		done:       make(chan bool),
 	}
 
 	return pool, nil
 }
 
-// Stop sends the done signal to the WorkerPool to clean up all Connections
-func (pool *WorkerPool) Stop() {
-	pool.done <- true
+// Start listens on events channel and tries to start a job and workers for every connection.
+func (pool *WorkerPool) Start() error {
+	for {
+		event := <-pool.events
+		if event.Type == watcher.Created {
+			job, err := newJob(pool.session, event.Connection, pool.log.Named("job"))
+			if err != nil {
+				pool.log.Debugw("creating new job failed", "error", err, "connectionID", event.ID)
+				continue
+			}
+
+			job.start()
+
+			pool.jobs[event.ID] = job
+			pool.numWorkers += event.Connection.Source.NumberOfWorkers()
+		}
+	}
 }
 
-// Start receives the number of worker goroutines from the main process
-func (pool *WorkerPool) Start() error {
-	// errors channel for workers to send back errors
-	// this will be used by the master to address any fails that come from a worker
-	errors := make(chan workerError)
+// Stop is a blocking function waiting for all jobs (and workers) to stop
+func (pool *WorkerPool) Stop() {
+	pool.log.Debugf("stopping all jobs")
 
-	// close channel for workers to send back normal exit
-	// simply dump the worker ID back on the channel
-	close := make(chan uint)
-
-	for {
-		select {
-		case <-pool.done:
-			// block & wait for the done signal
-			pool.log.Debugf("received the done signal")
-			for _, job := range pool.jobs {
-				for _, worker := range job.workers {
-					worker.done <- true
-				}
-
-				if err := job.mutex.Unlock(context.TODO()); err != nil {
-					pool.log.Errorw("unable to unlock", "error", err, "connectionID", job.connection.ID)
-				}
-				pool.log.Debugw("unlocked", "connectionID", job.connection.ID)
-			}
-			return nil
-		case event := <-pool.events:
-			if event.Type == watcher.Created {
-				// acquire lock
-				mutex := concurrency.NewMutex(pool.session, lockPrefix+string(event.ID))
-				pool.log.Debugw("locking...", "connectionID", event.ID)
-				ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-				err := mutex.Lock(ctx)
-				cancel()
-				if err != nil {
-					pool.log.Debugw("unable to lock", "error", err, "connectionID", event.ID)
-					continue
-				}
-				pool.log.Debugw("lock acquired", "connectionID", event.ID)
-
-				// start job
-				count := event.Connection.Source.NumberOfWorkers()
-				if _, ok := pool.jobs[event.Connection.ID]; !ok {
-					pool.jobs[event.Connection.ID] = &job{
-						connection: event.Connection,
-						workers:    make(map[uint]*worker),
-						mutex:      mutex,
-					}
-				}
-
-				for id := uint(0); id < count; id++ {
-					pool.assignWorker(id, event.Connection, errors, close)
-					pool.numWorkers++
-				}
-			}
-		case workerErr := <-errors:
-			// worker thread errored
-			// would need to figure out retry logic here
-			pool.log.Warnw("received an error from worker", "workerID", workerErr.id, "error", workerErr.err.Error(), "total", pool.numWorkers)
-			pool.numWorkers--
-			pool.removeWorker(workerErr)
-
-			pool.log.Debugf("restarting worker", "workerID", workerErr.id, "connectionID", workerErr.connectionID)
-			pool.assignWorker(workerErr.id, pool.jobs[workerErr.connectionID].connection, errors, close)
-			pool.numWorkers++
-		case workerID := <-close:
-			// worker thread closed normally
-			pool.log.Debugw("closing worker", "workerID", workerID)
-		}
+	for _, job := range pool.jobs {
+		job.stop()
 	}
 }
 
@@ -120,31 +71,73 @@ const lockPrefix = "serverless-event-gateway-connector/locks/connections/"
 
 // job is the interim struct to manage the specific worker for a give connectionID
 type job struct {
-	workers    map[uint]*worker
 	connection *connection.Connection
 	mutex      *concurrency.Mutex
+	workers    map[uint]*worker
+	done       chan bool // signal channel to stop all worker processes
+	waitGroup  *sync.WaitGroup
+	log        *zap.SugaredLogger
 }
 
-// removeWorker deducts the specified worker from the job, allowing it to be reassigned
-func (pool *WorkerPool) removeWorker(w workerError) {
-	delete(pool.jobs[w.connectionID].workers, w.id)
+// newJob creates new job an tries to lock the connection in etcd
+func newJob(session *concurrency.Session, conn *connection.Connection, log *zap.SugaredLogger) (*job, error) {
+	mutex := concurrency.NewMutex(session, lockPrefix+string(conn.ID))
+
+	log.Debugw("locking connection...", "connectionID", conn.ID)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	if err := mutex.Lock(ctx); err != nil {
+		return nil, err
+	}
+	log.Debugw("connection lock acquired", "connectionID", conn.ID)
+
+	return &job{
+		connection: conn,
+		mutex:      mutex,
+		workers:    make(map[uint]*worker),
+		done:       make(chan bool),
+		waitGroup:  &sync.WaitGroup{},
+		log:        log,
+	}, nil
 }
 
-// assignWorker processes the new event and signals the requisite worker goroutines
-func (pool *WorkerPool) assignWorker(id uint, conn *connection.Connection, errors chan<- workerError, close chan<- uint) {
-	pool.jobs[conn.ID].workers[id] = newWorker(id, conn, errors, close, pool.log)
+func (j *job) start() {
+	for id := uint(0); id < j.connection.Source.NumberOfWorkers(); id++ {
+		j.workers[id] = newWorker(id, j.connection, j.done, j.waitGroup, j.log.Named("worker"))
+		go j.workers[id].run()
+		j.waitGroup.Add(1)
+	}
 }
 
-func newWorker(id uint, conn *connection.Connection, errors chan<- workerError, close chan<- uint, log *zap.SugaredLogger) *worker {
+func (j *job) stop() {
+	j.log.Debugw("stopping a job", "connectionID", j.connection.ID)
+
+	j.done <- true
+	j.waitGroup.Wait()
+
+	if err := j.mutex.Unlock(context.TODO()); err != nil {
+		j.log.Errorw("unable to unlock connection", "error", err, "connectionID", j.connection.ID)
+	}
+	j.log.Debugw("connection unlocked", "connectionID", j.connection.ID)
+}
+
+// worker is the internal representation of the worker process
+type worker struct {
+	id         uint
+	connection *connection.Connection
+	done       <-chan bool
+	waitGroup  *sync.WaitGroup
+	log        *zap.SugaredLogger
+}
+
+func newWorker(id uint, conn *connection.Connection, done <-chan bool, wg *sync.WaitGroup, log *zap.SugaredLogger) *worker {
 	w := &worker{
 		id:         id,
-		done:       make(chan bool),
 		connection: conn,
-		errors:     errors,
-		close:      close,
+		done:       done,
+		waitGroup:  wg,
 		log:        log,
 	}
-	go w.run()
 	return w
 }
 
@@ -154,37 +147,12 @@ func (w *worker) run() {
 		select {
 		case <-w.done:
 			w.log.Debugw("trapped done signal", "workerID", w.id)
+			w.waitGroup.Done()
 			return
-
 		default:
 			// perform the actual connection here
-			for i := 0; i < 3; i++ {
-				w.log.Debugf("would be handling the stuff here: %d, %+v", w.id, w.connection)
-				time.Sleep(3 * time.Second)
-			}
-
-			// in case of error:
-			// w.errors <- workerError{id: w.id, connectionID: c.ID, err: err}
-			w.log.Debugw("worker finished job", "workerID", w.id, "connectionID", w.connection.ID)
-			w.close <- w.id
-			return
+			w.log.Debugw("would be handling the stuff here", "workerID", w.id, "connectionID", w.connection.ID)
+			time.Sleep(3 * time.Second)
 		}
 	}
-}
-
-// worker is the internal representation of the worker process
-type worker struct {
-	id         uint
-	connection *connection.Connection
-	errors     chan<- workerError
-	close      chan<- uint
-	done       chan bool
-	log        *zap.SugaredLogger
-}
-
-// workerError for cases where the worker ends up failing for a specific reason
-type workerError struct {
-	id           uint
-	connectionID connection.ID
-	err          error
 }
