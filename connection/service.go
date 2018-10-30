01249e6ac7689677a2e9662098fd24159418e235
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-10-30 09:35:00 +0100

    Add support for metadata (#34)

--- connection/service.go
+++ connection/service.go
@@ -1,8 +1,10 @@
 package connection
 
+import "github.com/serverless/event-gateway/metadata"
+
 // Service for managing connections.
 type Service interface {
-	ListConnections(space string) ([]*Connection, error)
+	ListConnections(space string, filters ...metadata.Filter) ([]*Connection, error)
 	CreateConnection(conn *Connection) (*Connection, error)
 	UpdateConnection(conn *Connection) (*Connection, error)
 	DeleteConnection(space string, id ID) error
